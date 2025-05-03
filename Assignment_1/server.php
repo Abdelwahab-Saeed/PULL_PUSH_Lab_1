9037a853979c87efcc53d2de6687a9d78322be83
Abdelwahab <abdelwahabsaeed415@gmail.com>
2025-05-03 06:29:15 +0300

    Alhamdullilah, task is finished

---
+++ Assignment_1/server.php
@@ -0,0 +1,23 @@
+<?php
+$products = [
+    ["id" => 1, "name" => "Laptop"],
+    ["id" => 2, "name" => "Keyboard"],
+    ["id" => 3, "name" => "Mouse"],
+    ["id" => 4, "name" => "Monitor"],
+    ["id" => 5, "name" => "Webcam"],
+    ["id" => 6, "name" => "External Hard Drive"],
+];
+
+$q = strtolower(trim($_GET['q'] ?? ''));
+$results = '';
+
+if (!empty($q)) {
+    foreach ($products as $product) {
+        if (strpos(strtolower($product['name']), $q) !== false) {
+            $results .= "<div>🛒 {$product['name']}</div>";
+        }
+    }
+}
+
+echo $results ?: "<div>No products found.</div>";
+?>
